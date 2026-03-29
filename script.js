c6271b69c7ff6c58c4a1f0694d275425448db26b
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2026-03-29 12:07:04 +0200

    Add grid structure

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+function createSquare() {
+    
+    const container = document.querySelector(".container");
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+}
+
+for (let i = 0; i < 256; i++) {
+    createSquare();
+}
